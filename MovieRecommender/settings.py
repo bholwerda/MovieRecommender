86cbaede2b3b69f7f7fa412bf1df9136673172cc
bholwerda <brett.holwerda@gmail.com>
2023-09-24 12:15:37 -0400

    hid secret key

--- MovieRecommender/settings.py
+++ MovieRecommender/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3e!@du7j8td6u4cu7j)mjqrlyomibhqe@t*d-+dg1ztif%%=j4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mydjangomovierecommender-887d599fa4e5.herokuapp.com']
 
 
 # Application definition
